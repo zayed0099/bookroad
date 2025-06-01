8b948d98d7a443977c21695750a83adefdca19f8
Zayed Ahmed <zayed.ah06@gmail.com>
2025-06-01 21:49:35 +0600

    fixed some minor issue

--- app/__init__.py
+++ app/__init__.py
@@ -8,43 +8,43 @@ db = SQLAlchemy()
 login_manager = LoginManager()
 
 def create_app():
-	app = Flask(__name__) # creating a flask instance
+    app = Flask(__name__) # creating a flask instance
 
-	app.secret_key = 'why_its_dark' # creating a secret key
+    app.secret_key = 'why_its_dark' # creating a secret key
 
-	basedir = os.path.abspath(os.path.dirname(__file__)) # getting the file directory of the code
+    basedir = os.path.abspath(os.path.dirname(__file__)) # getting the file directory of the code
 
-	# Telling the flask-sqlalchemy to place the db inside the url of the app
-	app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'app.db')
+    # Telling the flask-sqlalchemy to place the db inside the url of the app
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'app.db')
 
-	db.init_app(app) # adding the db inside the app
+    db.init_app(app) # adding the db inside the app
 
-	from app import models  # Import models from the current directory
-	from app.models import User
+    from app import models  # Import models from the current directory
+    from app.models import User
 
-	with app.app_context(): # creating all the database tables
-		db.create_all()
+    with app.app_context(): # creating all the database tables
+        db.create_all()
 
-	login_manager.init_app(app)
+    login_manager.init_app(app)
 
-	@login_manager.user_loader
-	def load_user(user_id):
-		try:
-			return User.query.get(int(user_id))
-		except Exception as e:
-			return None
+    @login_manager.user_loader
+    def load_user(user_id):
+        try:
+            return User.query.get(int(user_id))
+        except Exception as e:
+            return None
 
-	login_manager.login_view = 'login'
+    login_manager.login_view = 'login'
 
-	from app.routes import setup_routes
-	setup_routes(app)
+    from app.routes import setup_routes
+    setup_routes(app)
 
-	return app
+    return app
 
-	
+    
 
 
-	
+    
 
 
 
--- app/routes.py
+++ app/routes.py
@@ -10,73 +10,73 @@ from app.models import User, Books
 -home.html(default page) = done
 -signin.html = done
 -login.html = done
--dashboard.html	= done	
+-dashboard.html = done  
 '''
 
-def setup_routes():
-	@app.route("/home")
-	def home():
-		return render_template('home.html')
+def setup_routes(app):
+    @app.route("/home")
+    def home():
+        return render_template('home.html')
 
-	@app.route("/signin", methods=["GET", "POST"])
-	def signin():
-		form = SignInForm()
-		
-		if form.validate_on_submit():
-			username = form.username.data
-			password_txt = form.password.data 
-			email = form.email.data
+    @app.route("/signin", methods=["GET", "POST"])
+    def signin():
+        form = SignInForm()
+        
+        if form.validate_on_submit():
+            username = form.username.data
+            password_txt = form.password.data 
+            email = form.email.data
 
-			existing_user = User.query.filter_by(email=email).first()
+            existing_user = User.query.filter_by(email=email).first()
 
-			if existing_user:
-				flash('User already exists.')
-				return redirect(url_for('login'))
-			else:
-				hashed_pw = generate_password_hash(password_txt)
+            if existing_user:
+                flash('User already exists.')
+                return redirect(url_for('login'))
+            else:
+                hashed_pw = generate_password_hash(password_txt)
 
-				new_user_entry = User(username=username, email=email, password=hashed_pw)
-				
-				try:
-					db.session.add(new_user_entry)
-					db.session.commit()
-					flash('User registered successfully! Please Login now to access your account.')
-				except SQLAlchemyError as e:
-					db.session.rollback()
-					return jsonify({'message': 'Invalid input'})
+                new_user_entry = User(username=username, email=email, password=hashed_pw)
+                
+                try:
+                    db.session.add(new_user_entry)
+                    db.session.commit()
+                    flash('User registered successfully! Please Login now to access your account.')
+                except SQLAlchemyError as e:
+                    db.session.rollback()
+                    return jsonify({'message': 'Invalid input'})
 
-				return redirect(url_for('login'))
-		
-		else:
-			return render_template('signin.html', form=form)
+                return redirect(url_for('login'))
+        
+        else:
+            return render_template('signin.html', form=form)
 
-	@app.route('/login', methods=["GET", "POST"])
-	def login():
-		form = LoginForm()
+    @app.route('/login', methods=["GET", "POST"])
+    def login():
+        form = LoginForm()
 
-		if form.validate_on_submit():
-			email = form.email.data
-			password_txt = form.password.data
+        if form.validate_on_submit():
+            email = form.email.data
+            password_txt = form.password.data
 
-			verify_user = User.query.filter_by(email=email).first()
+            verify_user = User.query.filter_by(email=email).first()
 
-			if verify_user and check_password_hash(verify_user.password, password_txt):
-				login_user(verify_user)
-				return(redirect(url_for('dashboard')))
-			else:
-				flash("We couldn't log you in. Please double-check your login details")
-				return render_template('login.html', form=form)
+            if verify_user and check_password_hash(verify_user.password, password_txt):
+                login_user(verify_user)
+                return(redirect(url_for('dashboard')))
+            else:
+                flash("We couldn't log you in. Please double-check your login details")
+                return render_template('login.html', form=form)
+                
+        else:
+            return render_template('login.html', form=form)
 
-		else:
-			return render_template('login.html', form=form)
-
-	@app.route('/logout')  	# A simple function to logout user
-	@login_required
-	def logout():
-    	logout_user()
-    	return redirect(url_for('home'))
+    @app.route('/logout')   # A simple function to logout user
+    @login_required
+    def logout():
+        logout_user()
+        return redirect(url_for('home'))
 
     @app.route('/dashboard')
     @login_required
     def dashboard():
-    	return render_template('dashboard.html')
+        return render_template('dashboard.html')
